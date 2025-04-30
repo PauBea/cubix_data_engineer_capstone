4d58130a39e307543d459036d48f5fd5945886ff
Beata Paulanyi <paulanyi@aol.com>
2025-04-30 17:00:41 +0200

    sales transformation checked

--- src/cubix_data_engineer_capstone/etl/silver/sales.py
+++ src/cubix_data_engineer_capstone/etl/silver/sales.py
@@ -21,13 +21,13 @@ def get_sales(sales_raw: DataFrame) -> DataFrame:
     return (
         sales_raw
         .select(
-            sf.col("son").cast("date"),
-            sf.col("orderdate"),
+            sf.col("son"),
+            sf.col("orderdate").cast("date"),
             sf.col("pk").cast("int"),
             sf.col("ck").cast("int"),
             sf.col("dateofshipping").cast("date"),
             sf.col("oquantity").cast("int"),
         )
-        .withColumnRenamed(SALES_MAPPING)
+        .withColumnsRenamed(SALES_MAPPING)
         .dropDuplicates()
     )
---
+++ tests/etl/silver/test_sales.py
@@ -0,0 +1,64 @@
+from datetime import datetime
+
+import pyspark.sql.types as st
+import pyspark.testing as spark_testing
+from cubix_data_engineer_capstone.etl.silver.sales import get_sales
+
+
+def test_get_sales(spark):
+    """Positive test that the function get_sales
+    returns the expected DataFrame.
+
+    :param spark: _description_
+    """
+    test_data = spark.createDataFrame(
+        [
+            # include - sample to keep
+            ("son_1", "2017-01-03", "1", "2", "2017-02-03", "3", "extra_value"),
+            # exclude - duplicate
+            ("son_1", "2017-01-03", "1", "2", "2017-02-03", "3", "extra_value"),
+        ],
+        schema=[
+            "son",
+            "orderdate",
+            "pk",
+            "ck",
+            "dateofshipping",
+            "oquantity",
+            "extra_col"
+
+
+        ]
+
+    )
+
+    result = get_sales(test_data)
+
+    expected_schema = st.StructType(
+        [
+            st.StructField("SalesOrderNumber", st.StringType(), True),
+            st.StructField("OrderDate", st.DateType(), True),
+            st.StructField("ProductKey", st.IntegerType(), True),
+            st.StructField("CustomerKey", st.IntegerType(), True),
+            st.StructField("ShipDate", st.DateType(), True),
+            st.StructField("OrderQuantity", st.IntegerType(), True)
+
+        ]
+    )
+
+    expected = spark.createDataFrame(
+
+        [
+            (
+                "son_1",
+                datetime(2017, 1, 3),
+                1,
+                2,
+                datetime(2017, 2, 3),
+                3
+            )
+        ],
+        schema=expected_schema
+    )
+
+    spark_testing.assertDataFrameEqual(result, expected)
